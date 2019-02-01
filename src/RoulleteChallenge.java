972da6950eab8061674816194f51ac89367fcf31
ajkmonster <phimphachanh.momo.victor@gmail.com>
2019-02-01 16:01:40 -0500

    added random in correct spot

--- src/RoulleteChallenge.java
+++ src/RoulleteChallenge.java
@@ -18,11 +18,12 @@ public class RoulleteChallenge {
         int[] arrhigh = {19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};
         int[] arrodd = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35};
         int[] arreven = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36};
-        Random r = new Random();
-        int b = (r.nextInt(1));
-        int a = (r.nextInt(35));
+
         System.out.println("Welcome to Roulette!");
         do {
+            Random r = new Random();
+            int b = (r.nextInt(1));
+            int a = (r.nextInt(35));
             Scanner key = new Scanner(System.in);
             System.out.print("Enter the type of bet you would like to place " +
                     "(Red|Black|Even|Odd|High|Low): ");
